2b525168feca1ebe4318b691e678748b13e64d06
TroyMoses <mugabimoses07@gmail.com>
2024-06-18 10:46:08 +0800

    products

--- app/_app.tsx
+++ app/_app.tsx
@@ -1,10 +1,17 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { Refine, Authenticated, AuthProvider } from "@refinedev/core";
-import RefineSnackbarProvider, { ThemedLayoutV2 } from "@refinedev/antd";
+import { ThemedLayoutV2 } from "@refinedev/antd";
 import dataProvider from "@refinedev/simple-rest";
 import { useNotificationProvider } from "@refinedev/antd";
 import { BrowserRouter } from "react-router-dom";
+import { SnackbarProvider } from 'material-ui-snackbar-provider'
+import routerProvider, {
+    CatchAllNavigate,
+    DocumentTitleHandler,
+    NavigateToResource,
+    UnsavedChangesNotifier,
+  } from "@refinedev/react-router-v6";
 
 const API_URL = "/api";
 
@@ -12,28 +19,32 @@ const authProvider: AuthProvider = {
   login: async ({ username, password }) => {
     if (username === "admin" && password === "admin") {
       localStorage.setItem("auth", "true");
-      return Promise.resolve();
+      return Promise.resolve({ success: true });
     }
-    return Promise.reject();
+    return Promise.reject({ success: false });
   },
   logout: async () => {
     localStorage.removeItem("auth");
-    return Promise.resolve();
+    return Promise.resolve({ success: true });
   },
-  checkError: async () => Promise.resolve(),
-  checkAuth: async () => {
-    return localStorage.getItem("auth") ? Promise.resolve() : Promise.reject();
+  check: async () => {
+    return localStorage.getItem("auth") ? Promise.resolve({ authenticated: true }) : 
+    Promise.reject({ authenticated: false });
   },
-  getPermissions: async () => Promise.resolve(),
+  getPermissions: async () => Promise.resolve({ success: true }),
+  onError: async (error) => {
+    console.error(error);
+    return Promise.resolve({ redirectTo: "/login" });
+  },
 };
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <RefineSnackbarProvider>
+    <SnackbarProvider SnackbarProps={{ autoHideDuration: 4000 }}>
       <BrowserRouter>
         <Refine
           dataProvider={dataProvider(API_URL)}
-          notificationProvider={notificationProvider}
+          notificationProvider={useNotificationProvider}
           authProvider={authProvider}
           routerProvider={routerProvider}
           resources={[
@@ -49,7 +60,7 @@ function MyApp({ Component, pageProps }: AppProps) {
           <Component {...pageProps} />
         </Refine>
       </BrowserRouter>
-    </RefineSnackbarProvider>
+    </SnackbarProvider>
   );
 }
 
---
+++ app/products/index.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { useDataGrid, List } from "@refinedev/mui";
+import { DataGrid, GridColDef } from "@mui/x-data-grid";
+
+interface IPost {
+    id: number;
+    name: string;
+  }
+
+const ProductList: React.FC = () => {
+  const { dataGridProps } = useDataGrid();
+  return (
+    <List>
+      <DataGrid {...dataGridProps} columns={[
+        { field: "id", headerName: "ID", type: "number" },
+        { field: "name", headerName: "Name", flex: 1 },
+      ]} />
+    </List>
+  );
+};
+
+export default ProductList;
